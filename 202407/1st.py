dbf01516e677c147e56d5a1dcb841013561a8b68
Zhang Jiahao <zjhkxxszy@gmail.com>
2025-07-08 09:48:32 +0800

    add def

--- 202407/1st.py
+++ 202407/1st.py
@@ -4,3 +4,6 @@ print(message)
 message = 'hello python crash course world'
 print(message)
 
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
